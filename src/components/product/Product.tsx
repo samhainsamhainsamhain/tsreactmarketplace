99d285a802e736c211351046266f6014c00f29bb
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-12 12:01:37 +0300

    fixed card and product-component styles, removed descriptions of product from products page

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -11,20 +11,21 @@ export default function ProductItem(props: ProductProps) {
   return (
     <Card>
       <div className={classes.product}>
-        <img
-          className={classes.image}
-          src={props.image}
-          alt="image of product"
-        />
+        <div className={classes.imageContainer}>
+          <img
+            className={classes.image}
+            src={props.image}
+            alt="image of product"
+          />
+        </div>
         <div className={classes.productInfo}>
           <span className={classes.title}>{props.title}</span>
-          <span className={classes.price}>{props.price}</span>
-        </div>
-        <p className={classes.description}>{props.description}</p>
-        <div className={classes.control}>
-          <button>-</button>
-          <span>11</span>
-          <button>+</button>
+          <span className={classes.price}>${props.price}</span>
+          <div className={classes.control}>
+            <button>-</button>
+            <span>11</span>
+            <button>+</button>
+          </div>
         </div>
       </div>
     </Card>
